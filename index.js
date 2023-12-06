44016d5eb64c019c0b35bb8113b286b16eca0763
husanjonmusayev <husanjonm279@gmail.com>
2023-12-06 23:51:10 +0500

    uy ishi

---
+++ index.js
@@ -0,0 +1,193 @@
+// 1 masala
+// let n = +prompt('sonni kiriting')
+// let sum=0;
+
+// for (let i = 1; i<=n; i++){
+//     if(i%2==0){
+//         sum += i;
+//     }
+// }
+// console.log(sum);
+
+// 2 masala
+// let n = +prompt('sonni kiriting')
+// let sum = 1;
+// for(let i =1; i<=n; i++){
+//     if(i%3==0){
+//         sum*=i
+//     }
+// }
+// console.log(sum);
+
+// 3 masala
+// let a = +prompt('a sonini kiriting')
+// let b = +prompt('b sonini kiriting')
+// for(let i =a; i<=b; i++){
+//     if(i%3==0 && i%7==0){
+//         console.log(i);
+//     }
+// }
+
+// 5 masala
+// let n = 10   //+prompt('sonni kiriting')
+// let sum =0;
+// for(let i =1; i<=n; i++){
+// sum += i;
+// }
+// console.log(sum);
+
+// 6 masala
+// let n = 456;
+// let y = Math.trunc(n / 100)
+// let o = Math.trunc(n / 10 % 10);
+// let b = n % 10;
+// let sum = ` `
+// switch (y) {
+//     case 1:
+//         sum += `bir yuz `
+//         break;
+
+//     case 2:
+//         sum += `ikki yuz `
+//         break;
+
+//     case 3:
+//         sum += `uch yuz `
+//         break;
+
+//     case 4:
+//         sum += `to'rt yuz `
+//         break;
+
+//     case 5:
+//         sum += `besh yuz `
+//         break;
+
+//     case 6:
+//         sum += `oltiyuz yuz `
+//         break;
+
+//     case 7:
+//         sum += `yetti yuz `
+//         break;
+
+//     case 8:
+//         sum += `sakizyuz yuz `
+//         break;
+
+//     case 9:
+//         sum += `to'izyuz yuz `
+//         break;
+
+//     default:
+//         sum = "Ushbu operatsiani amalga oshirish mumkun emas"
+//         break;
+// }
+// switch (o) {
+//     case 1:
+//         sum += `o'n `
+//         break;
+
+//     case 2:
+//         sum += `yigirma `
+//         break;
+
+//     case 3:
+//         sum += `o'ttiz `
+//         break;
+
+//     case 4:
+//         sum += `qirq `
+//         break;
+
+//     case 5:
+//         sum += `ellik `
+//         break;
+
+//     case 6:
+//         sum += `oltmush `
+//         break;
+
+//     case 7:
+//         sum += `yetmush `
+//         break;
+
+//     case 8:
+//         sum += `sakson `
+//         break;
+
+//     case 9:
+//         sum += `to'son `
+//         break;
+
+//     default:
+//         sum = "Ushbu operatsiani amalga oshirish mumkun emas"
+//         break;
+// }
+// switch (b) {
+//     case 1:
+//         sum += `bir `
+//         break;
+
+//     case 2:
+//         sum += `ikki `
+//         break;
+
+//     case 3:
+//         sum += `uch `
+//         break;
+
+//     case 4:
+//         sum += `tort `
+//         break;
+
+//     case 5:
+//         sum += `besh `
+//         break;
+
+//     case 6:
+//         sum += `olti `
+//         break;
+
+//     case 7:
+//         sum += `yetti `
+//         break;
+
+//     case 8:
+//         sum += `sakkiz `
+//         break;
+
+//     case 9:
+//         sum += `to'qqiz `
+//         break;
+
+//     default:
+//         sum = "Ushbu operatsiani amalga oshirish mumkun emas"
+//         break;
+// }
+// console.log(sum);
+
+// 7 masala
+// let a = 5;
+// let b = 6;
+
+// let o = "+"; // * / - 
+
+// let sum = 0;
+// if (o == "+") {
+//     sum += a + b;
+// }
+// if (o == "-") {
+//     sum += a - b;
+// }
+// if (o == "*") {
+//     sum += a * b;
+// }
+// if(o == "/"){
+//     sum += a / b;
+//     }
+
+// console.log(sum);
+    
+
+
